84eba20f6d2f09dc16047c1ad9c7800301aaceb3
junny0442 <99376382+junny0442@users.noreply.github.com>
2023-02-13 21:30:57 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://playentry.org/uploads/0n/dh/0ndh4e52l600zsox1n2u9840fbcjs4sd.svg'
+    })
+
+}, 500)
